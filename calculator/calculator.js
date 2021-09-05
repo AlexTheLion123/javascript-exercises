42dd804797bb4ebf0b6092cbcf05ccf004e67e05
Alex <alexurban12@gmail.com>
2021-09-06 01:39:24 +0200

    calculator solved

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length == 0){
+    return 0;
+  }
+  
+  return arr.reduce((sum, current) => sum + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num == 0){
+    return 1;
+  }
+  let product = 1;
+  while(num > 0){
+    product = product * num;
+    num--;
+  }
+  return product;
 };
 
 module.exports = {
