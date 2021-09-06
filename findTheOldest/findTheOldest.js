d9ba17c4bf4b742069f8d2f74ffaeb1e469442fe
Alex <alexurban12@gmail.com>
2021-09-06 15:13:20 +0200

    findTheOldest complete

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest,current) => {
+        const currentAge = getAge(current);
+        const oldestAge = getAge(oldest);
+        return oldestAge < currentAge ? current : oldest; 
+    })
 };
+
+function getAge(person){
+    if(!person.yearOfDeath){
+        person.yearOfDeath = (new Date()).getFullYear()
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 module.exports = findTheOldest;
