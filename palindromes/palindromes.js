95418e5944c5ffca06d944af3ade59f9a7d4ba8a
Alex <alexurban12@gmail.com>
2021-09-06 13:53:43 +0200

    palindromes complete

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    str = str.replace(" ", "");
 
+    tempArr = str.split('');
+    tempArr = tempArr.filter(item => item != " " );
+    arr = tempArr.map(item => item.toLowerCase());
+    orig = arr.join("");
+    
+    let reverse = [];
+    for(let i = arr.length-1; i>=0; i--){
+        reverse.push(arr[i]);
+    }
+    
+    const answer = reverse.join("");
+    return answer == orig;
 };
+
+palindromes("Hellothere AdsfkjD")
 
 module.exports = palindromes;
