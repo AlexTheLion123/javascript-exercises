dcef56bf7c0113d7ed0a2e432d0038d4bc08b4f2
Alex <alexurban12@gmail.com>
2021-09-06 14:06:20 +0200

    fibonnaci complete

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n<0){
+        return "OOPS";
+    }
+    
+    if(typeof n == "number"){
+        n = parseInt(n);
+    }
 
+    if(n==1 || n==2){
+        return 1;
+    }
+    
+    let previous = 1;
+    let current = 1;
+    let result = 0;
+    let counter = 1;
+
+    while(counter < n-1){
+        result = previous + current;
+        previous = current;
+        current = result;
+        counter++;
+    }
+    return result;
 };
 
 module.exports = fibonacci;
